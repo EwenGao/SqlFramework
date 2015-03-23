de32f80f6c511083029595f9d14c0cd841aca805
EwenGao <jayniszhde@126.com>
2015-03-23 17:02:30 +0800

    Create SqlQueryable.cs
    数据实体操作类

---
+++ SqlQueryable.cs
@@ -0,0 +1,19 @@
+
+using System.Collections.Generic;
+
+namespace SqlFramework
+{
+    public class SqlQueryable<T>
+    {
+        public string OrderBy { get; set; }
+
+        public string Where { get; set; }
+
+        public int Skip { get; set; }
+
+        public int PageSize { get; set; }
+
+        public List<string> Include { get; set; }
+
+    }
+}
