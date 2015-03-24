c2463a3c7921064325b8ce4d1e285f174f007fd2
EwenGao <jayniszhde@126.com>
2015-03-24 14:48:04 +0800

    数据库操作类

--- SqlCore.cs
+++ SqlCore.cs
@@ -1,10 +1,7 @@
-using System;
-using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
 
 namespace SqlFramework
 {
